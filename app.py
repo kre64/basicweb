53fbe26f864fdc09a18677f59d582e2bbd5ad2d3
kre64 <kre9404@gmail.com>
2018-08-29 15:27:14 -0400

    route for login added

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+#route for homepage
 @app.route('/')
 def home():
     return render_template('home.html')
 
+#route for login page
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+#what does this actually do?
 if __name__ == '__main__':
 	app.run()
+
+#add threaded, os stuff
